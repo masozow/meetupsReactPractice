ab69bee7b65ebaa9018d6d17c8ef951b2826b43f
masozow <14838605+masozow@users.noreply.github.com>
2021-06-27 21:35:12 -0600

    added nav to navlinks

--- src/components/menu/NavLinks.js
+++ src/components/menu/NavLinks.js
@@ -6,22 +6,23 @@ import classes from './NavLinks.module.css';
 function NavLinks(props) {
     const favoritesCtx = useContext(FavoritesContext);
     return (
-        <ul className={props.open ? classes.mobileMenu : ''}>
-            <li key={1}>
-                <NavLink to='/' exact activeClassName={classes.active}>All meetups</NavLink>
-            </li>
-            <li key={2}>
-                <NavLink to='/new-meetup' activeClassName={classes.active}>New meetup</NavLink>
-            </li>
-            <li key={3}>
-                <NavLink to='/favorites' activeClassName={classes.active}>Favorites
+        <nav>
+            <ul className={props.open ? classes.mobileMenu : ''}>
+                <li key={1}>
+                    <NavLink to='/' exact activeClassName={classes.active}>All meetups</NavLink>
+                </li>
+                <li key={2}>
+                    <NavLink to='/new-meetup' activeClassName={classes.active}>New meetup</NavLink>
+                </li>
+                <li key={3}>
+                    <NavLink to='/favorites' activeClassName={classes.active}>Favorites
                             <span className={classes.badge}>
-                        {favoritesCtx.totalFavorites}
-                    </span>
-                </NavLink>
-            </li>
-        </ul>
-
+                            {favoritesCtx.totalFavorites}
+                        </span>
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
     );
 }
 
